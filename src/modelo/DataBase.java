3031cf13398e4d7a647756bb25c4d960070c3be6
Gonzalo <Gonzalo@192.168.0.19>
2018-06-17 10:43:48 +0200

    Acabado

--- src/modelo/DataBase.java
+++ src/modelo/DataBase.java
@@ -1,9 +1,11 @@
 package modelo;
 
+import vista.Vista;
 
 public class DataBase {
 
     public static void main(String[] args) {
-
+        Vista frame = new Vista();
+        frame.setVisible(true);
     }
 }
